8f16a5fdd061cdcda17a20c7f89144c338f958ea
vladimiriusus <tatar-vova@ukr.net>
2021-11-04 21:37:08 +0200

    new

--- start/js/main.js
+++ start/js/main.js
@@ -1,3 +1,12 @@
 $(function(){
+	$('.popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
 
 });
