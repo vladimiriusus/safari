d5a810e1097d26e6df79918dc1363bbf094b0d63
vladimiriusus <tatar-vova@ukr.net>
2021-11-05 19:11:50 +0200

    1

--- start/js/main.js
+++ start/js/main.js
@@ -9,4 +9,9 @@ $(function(){
 		fixedContentPos: false
 	});
 
+	$('.galerry__slider').slick({
+		prevArrow:'<button type="button" class="slick-btn slick-prew"><img src="images/arrow-left.svg" alt=""></button>',
+		nextArrow:'<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+	});
+
 });
