fa88b5e6fe7dd3aed85cd93a2d255a334017ce38
vladimiriusus <tatar-vova@ukr.net>
2021-11-17 11:57:11 +0200

    works

--- start/js/main.js
+++ start/js/main.js
@@ -13,5 +13,22 @@ $(function(){
 		prevArrow:'<button type="button" class="slick-btn slick-prew"><img src="images/arrow-left.svg" alt=""></button>',
 		nextArrow:'<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt=""></button>',
 	});
+	$('.galerry__item-inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
 
 });
